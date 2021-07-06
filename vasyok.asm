3bd6fc7c3127d6472732cd202dd44ad7f1d9447a
lfcad4 <87039662+lfcad4@users.noreply.github.com>
2021-07-06 22:40:15 +0300

    Add files via upload

---
+++ vasyok.asm
@@ -0,0 +1,225 @@
+section .text
+use16
+org 0x7c00
+start:
+section .data
+
+mov ax,3
+int 10h
+
+mov ah,0x0E
+mov al,'w'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,'l'
+int 10h
+mov ah,0x0E
+mov al,'c'
+int 10h
+mov ah,0x0E
+mov al,'o'
+int 10h
+mov ah,0x0E
+mov al,'m'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'t'
+int 10h
+mov ah,0x0E
+mov al,'o'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'W'
+int 10h
+mov ah,0x0E
+mov al,'a'
+int 10h
+mov ah,0x0E
+mov al,'l'
+int 10h
+mov ah,0x0E
+mov al,'r'
+int 10h
+mov ah,0x0E
+mov al,'u'
+int 10h
+mov ah,0x0E
+mov al,'s'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'k'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,'r'
+int 10h
+mov ah,0x0E
+mov al,'n'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,'l'
+int 10h
+
+mov ah,0x0E
+mov al,'~'
+int 10h
+mov ah,0x0E
+mov al,'w'
+int 10h
+mov ah,0x0E
+mov al,'l'
+int 10h
+mov ah,0x0E
+mov al,'r'
+int 10h
+mov ah,0x0E
+mov al,'@'
+int 10h
+mov ah,0x0E
+mov al,'c'
+int 10h
+mov ah,0x0E
+mov al,'o'
+int 10h
+mov ah,0x0E
+mov al,'n'
+int 10h
+mov ah,0x0E
+mov al,'s'
+int 10h
+mov ah,0x0E
+mov al,'o'
+int 10h
+mov ah,0x0E
+mov al,'l'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,'#'
+int 10h
+mov ah,0x0E
+mov al,'>'
+int 10h
+
+
+mov ah,0
+
+
+mov ah,0x0E
+mov al,'T'
+int 10h
+mov ah,0x0E
+mov al,'h'
+int 10h
+mov ah,0x0E
+mov al,'a'
+int 10h
+mov ah,0x0E
+mov al,'n'
+int 10h
+mov ah,0x0E
+mov al,'k'
+int 10h
+mov ah,0x0E
+mov al,'s'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'f'
+int 10h
+mov ah,0x0E
+mov al,'o'
+int 10h
+mov ah,0x0E
+mov al,'r'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'u'
+int 10h
+mov ah,0x0E
+mov al,'s'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'t'
+int 10h
+mov ah,0x0E
+mov al,'h'
+int 10h
+mov ah,0x0E
+mov al,'i'
+int 10h
+mov ah,0x0E
+mov al,'s'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'s'
+int 10h
+mov ah,0x0E
+mov al,'y'
+int 10h
+mov ah,0x0E
+mov al,'s'
+int 10h
+mov ah,0x0E
+mov al,'t'
+int 10h
+mov ah,0x0E
+mov al,'e'
+int 10h
+mov ah,0x0E
+mov al,'m'
+int 10h
+mov ah,0x0E
+mov al,' '
+int 10h
+mov ah,0x0E
+mov al,'<'
+int 10h
+mov ah,0x0E
+mov al,'3'
+int 10h
+
+
+int 16h
+mov ah, 0x0e
+int 10h
+
+jmp $
+
+
